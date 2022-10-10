2aae2349e396aba497ca46ad3a4bb1e52b929cbe
MD-Baki <bakimd77@gmail.com>
2022-10-11 03:47:37 +0600

    add font awesome

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,5 @@
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Home.css'
 
@@ -10,7 +12,7 @@ const Home = () => {
                     <div className='text-white'>
                         <h1 className='font-bold text-3xl'>Center for <span className='text-[#5fe0e9]'>Digital Education.</span></h1>
                         <p className='w-7/12 py-5'>If you want to expand your knowledge, quiz occupies an important place. So if you want to see the extent of your knowledge then stay with us by answering the simple quiz on our site.</p>
-                        <button className='bg-[#2b3351] font-bold px-5 py-2 rounded-lg'>Start Larning</button>
+                        <button className='bg-[#2b3351] font-bold text-lg px-5 py-2 rounded-lg'>Start Larning <FontAwesomeIcon icon={faAnglesRight}></FontAwesomeIcon> </button>
                     </div>
                 </div>
             </div>
