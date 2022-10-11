b8e25ee1e40e70355df29368cb3bcc7403b77fe7
MD-Baki <bakimd77@gmail.com>
2022-10-11 16:20:22 +0600

    q and a add our site

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -9,12 +9,14 @@ const QuizDetails = () => {
     return (
         <div className="md:w-11/12 lg:w-9/12 mx-auto py-20">
             <h2 className='col-span-4 font-bold text-3xl text-[#2b3351] pb-3 text-center'>Quiz of {quizList.name}</h2>
-            {
-                question.map(ques => <QuizTotal
-                    key={ques.id}
-                    ques={ques}
-                ></QuizTotal>)
-            }
+            <div className='grid lg:grid-cols-2 gap-10 mt-5'>
+                {
+                    question.map(ques => <QuizTotal
+                        key={ques.id}
+                        ques={ques}
+                    ></QuizTotal>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/QuizTotal/QuizTotal.js
+++ src/components/QuizTotal/QuizTotal.js
@@ -1,11 +1,23 @@
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './QuizTotal.css'
 
 const QuizTotal = ({ ques }) => {
     console.log(ques);
-    const { question } = ques
+    const { question, options } = ques
     return (
-        <div>
-            <p>{question}</p>
+        <div className='quiz-container bg-[#edeff8] border-2 border-[#dce2ff]  text-[#2b3351] rounded-xl p-5'>
+            <div className='flex justify-between items-center'>
+                <h4 className='font-bold pb-3'>Quiz: {question}</h4>
+                <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+            </div>
+            <ol className='option grid grid-cols-2 gap-2 mt-2'>
+                <li>1. {options[0]}</li>
+                <li>2. {options[1]}</li>
+                <li>3. {options[2]}</li>
+                <li>4. {options[3]}</li>
+            </ol>
         </div>
     );
 };
