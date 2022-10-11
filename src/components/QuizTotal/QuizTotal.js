4f4efca14d2543d172e5180aae4401a334a2d355
MD-Baki <bakimd77@gmail.com>
2022-10-12 03:11:24 +0600

    add dynamic quiz option

--- src/components/QuizTotal/QuizTotal.js
+++ src/components/QuizTotal/QuizTotal.js
@@ -6,9 +6,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const QuizTotal = ({ ques }) => {
-    console.log(ques);
     const { question, options, correctAnswer } = ques
-
+    // console.log(options);
     const ans = () => {
         toast.success(correctAnswer, {
             position: "top-center"
@@ -21,12 +20,16 @@ const QuizTotal = ({ ques }) => {
                 <h4 className='font-bold pb-3'>Quiz: {question}</h4>
                 <button className='ml-2' onClick={ans}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
             </div>
-            <ol className='option grid grid-cols-2 gap-2 mt-2'>
-                <li>1. {options[0]}</li>
-                <li>2. {options[1]}</li>
-                <li>3. {options[2]}</li>
-                <li>4. {options[3]}</li>
-            </ol>
+            <div className='option grid grid-cols-2 gap-2 mt-2'>
+                {
+                    options.map((data) => (
+                        <div className='bg-white py-2 px-3 rounded-lg' key={data}>
+                            <input type="radio"></input><span className='pl-2'>{data}</span>
+                        </div>
+
+                    ))
+                }
+            </div>
             <ToastContainer></ToastContainer>
         </div>
     );
