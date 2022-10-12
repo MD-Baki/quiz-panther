79b2905137bb6cbe9d05eb0ee744a2a67ffeff01
MD-Baki <bakimd77@gmail.com>
2022-10-12 17:04:38 +0600

    simple change

--- src/components/QuizTotal/QuizTotal.js
+++ src/components/QuizTotal/QuizTotal.js
@@ -1,18 +1,12 @@
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck, faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import './QuizTotal.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const QuizTotal = ({ ques }) => {
-    const { question, options, correctAnswer } = ques
-
-    const ans = () => {
-        toast.success(correctAnswer, {
-            position: "top-center"
-        });
-    }
+    const { question, options, correctAnswer, id } = ques
 
     const set = (data) => {
         if (data === correctAnswer) {
@@ -22,22 +16,29 @@ const QuizTotal = ({ ques }) => {
         }
     }
 
+    // const ans = () => {
+    //     console.log('clicked');
+    // }
+
+    const [open, setOpen] = useState(false)
+
     return (
         <div className='quiz-container bg-[#edeff8] border-2 border-[#dce2ff]  text-[#2b3351] rounded-xl p-5'>
             <div className='flex justify-between items-center'>
                 <h4 className='font-bold pb-3'>Quiz: {question}</h4>
-                <button className='ml-2' onClick={ans}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
+                <button className='ml-2'
+                    onClick={() => setOpen(!open)}
+                ><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
             </div>
             <div className='option grid grid-cols-2 gap-2 mt-2'>
                 {
                     options.map(data => (
-                        <div className='bg-white py-2 px-3 rounded-lg' key={data}>
+                        <div className='bg-white py-2 px-3 rounded-lg' key={data} id={id}>
                             <input
                                 type="radio"
                                 id={data}
-                                name='react-radio'
+                                name={id}
                                 value={data}
-
                             />
                             <label
                                 onClick={() => set(data)}
@@ -49,6 +50,9 @@ const QuizTotal = ({ ques }) => {
 
                     ))
                 }
+            </div>
+            <div className={`${open ? 'block' : 'hidden'}`}>
+                <p className='font-bold bg-white py-1 px-2 mt-2 text-lime-700 rounded-lg'> <FontAwesomeIcon className='pr-2' icon={faCircleCheck}></FontAwesomeIcon> {correctAnswer}</p>
             </div>
             <ToastContainer></ToastContainer>
         </div>
